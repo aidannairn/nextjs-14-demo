14b836503ef81cca2afb52921673219abd4ac5c2
aidannairn <aidan.nairn95@gmail.com>
2023-11-02 13:49:39 +1300

    feat: CREATE new invoice functionality has been implemented.

---
+++ app/lib/actions.ts
@@ -0,0 +1,39 @@
+'use server'
+import { redirect } from 'next/navigation'
+import { revalidatePath } from 'next/cache'
+import { sql } from '@vercel/postgres'
+import { z } from 'zod'
+
+const InvoiceSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+})
+
+const CreateInvoice = InvoiceSchema.omit({ id: true, date: true })
+
+export async function createInvoice(formData: FormData) {
+  /* const rawFormData = {
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  } // Can be refactored to the following line */
+
+  const rawFormData = Object.fromEntries(formData.entries())
+  const { customerId, amount, status } = CreateInvoice.parse(rawFormData)
+
+  /* It's usually good practice to store monetary values in cents in your database to eliminate JavaScript floating-point errors and ensure greater accuracy.  */
+  const amountInCents = amount * 100
+
+  const date = new Date().toISOString().split('T')[0]
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `
+
+  revalidatePath('/dashboard/invoices')
+  redirect('/dashboard/invoices')
+}
